6a6b5066660ef88f4f8afb51dcdef41771b730d9
Matthew Klein <ggallin08@gmail.com>
2014-11-26 17:50:02 -0500

    added more to simple_steering.cpp. Does not compile.

--- src/simple_steering.cpp
+++ src/simple_steering.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv){
 
   pub_vel = nh.advertise<geometry_msgs::Twist>("/robot0/cmd_vel",1); //This is the advertiser object that will publish the desired forward and angular velocity of the robot.
 
-  tf::TransformListener(nh) tf;
+  tf::TransformListener listener;
 
   // Initialize the velocity command that we will publish.
   geometry_msgs::Twist vel;
@@ -18,3 +18,24 @@ int main(int argc, char** argv){
   vel.angular.x = 0;
   vel.angular.y = 0;
   vel.angular.z = 0;
+
+  ros::Rate rate(10.0);
+
+  while (nh.ok(){
+      tf::StampedTransform transform;
+      try{
+	listener.lookupTransform("/robot0","/map",ros::Time(0),transform);
+      }
+      catch (tf::TransformException ex){
+	ROS_ERROR("%s",ex.what());
+	ros::Duration(1.0).sleep();
+      }
+
+      vel.angular.z = 4.0 * atan2(transform.getOrigin().y(),transform.getOrigin().x());
+      vel.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(),2)+pow(transform.getOrigin().y(),2));
+
+      pub_vel.publish(vel);
+      rate.sleep();
+    }
+    return 0;
+};
