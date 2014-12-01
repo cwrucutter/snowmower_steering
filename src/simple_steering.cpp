ab15d44c33c269c305e113e96f99e1a61fe198b3
Matthew Klein <ggallin08@gmail.com>
2014-12-01 15:50:06 -0500

    finished simple_steering with transforms and made a node that broadcasts a goal transform based on an input from rviz

--- src/simple_steering.cpp
+++ src/simple_steering.cpp
@@ -1,12 +1,13 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include <tf/tf.h>
+#include <geometry_msgs/Pose.h>
+#include <tf/transform_listener.h>
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "simple_steering");
   ros::NodeHandle nh;
 
-  pub_vel = nh.advertise<geometry_msgs::Twist>("/robot0/cmd_vel",1); //This is the advertiser object that will publish the desired forward and angular velocity of the robot.
+  ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/robot0/cmd_vel",1); //This is the advertiser object that will publish the desired forward and angular velocity of the robot.
 
   tf::TransformListener listener;
 
@@ -19,12 +20,14 @@ int main(int argc, char** argv){
   vel.angular.y = 0;
   vel.angular.z = 0;
 
+
+  tf::StampedTransform transform;
+
   ros::Rate rate(10.0);
 
-  while (nh.ok(){
-      tf::StampedTransform transform;
+  while (nh.ok()){
       try{
-	listener.lookupTransform("/robot0","/map",ros::Time(0),transform);
+	listener.lookupTransform("/goalPose","/robot0",ros::Time(0),transform);
       }
       catch (tf::TransformException ex){
 	ROS_ERROR("%s",ex.what());
@@ -34,8 +37,8 @@ int main(int argc, char** argv){
       vel.angular.z = 4.0 * atan2(transform.getOrigin().y(),transform.getOrigin().x());
       vel.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(),2)+pow(transform.getOrigin().y(),2));
 
-      pub_vel.publish(vel);
+      vel_pub.publish(vel);
       rate.sleep();
     }
-    return 0;
-};
+  return 0;
+}
