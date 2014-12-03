b444f130af5e4f3cb21de04cba42bd8c0ff1cfe1
Matthew Klein <ggallin08@gmail.com>
2014-12-02 20:27:45 -0500

    Changed PoseStamped_to_TF.cpp to update 10 times a second. Not sure if this is necessary. Also made the goalPose transform to initialize as the robot0 transform. Also added lots of comments.

--- src/PoseStamped_to_TF.cpp
+++ src/PoseStamped_to_TF.cpp
@@ -1,23 +1,15 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
+// Make a global PoseStamped variable. This is reassigned by goalPose_CB everytime rviz publishes a PoseStamped when the "2D Nav Goal" button is pressed.
+geometry_msgs::PoseStamped g_pose;
 
 //rviz has a button that says "2D Nav Goal". When this button is pressed, a PoseStamped message is published to the topic "/move_base_simple/goal". This callback function is called when a message is published to that topic, converts the PoseStamped message into a stamped transform, and broadcasts it. 
 void goalPose_CB(const geometry_msgs::PoseStamped& msg){
-  //Create a broadcaster object (should this be done once in main?)
-  static tf::TransformBroadcaster br;
-
-  //Create a transform object (should this be done once in main?)
-  tf::Transform transform;
-
-  //Set the origin of the transform using the x,y,z members of msg.pose.position. msg is the PoseStamped that's being passed into the callback funtion.
-  transform.setOrigin(tf::Vector3(msg.pose.position.x, msg.pose.position.y, 0.0));
-
-  //Set the orientation of the transform using the x,y,z,w members msg.pose.orientation. msg is the PoseStamped that's being passed into the callback funtion.
-  transform.setRotation(tf::Quaternion(msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z,msg.pose.orientation.w));
-
-  //Finally, broadcast the Transform. It is possible that this should be done over and over at a specific rate.
-  br.sendTransform(tf::StampedTransform(transform, msg.header.stamp,"map_static","goalPose"));
+  //Write published PoseStamped to the global variable
+  g_pose = msg;
 }
 
 int main(int argc, char** argv){
@@ -28,9 +20,60 @@ int main(int argc, char** argv){
   //Create a subscriber object which subscribes to the topic that rviz publishes to when a "2D Nav Goal" is selected.
   ros::Subscriber goalPose_sub = nh.subscribe("/move_base_simple/goal",1,goalPose_CB);
 
+  tf::TransformListener listener;
+
+  tf::StampedTransform tf_temp;
+
+  //This variable indicates whether a transform was found or not.
+  bool test = true;
+  while (nh.ok() && test){
+    try{
+      //set the test to false, which will break the loop! A transform was found!
+      test = false;
+      listener.lookupTransform("/map", "/robot0",ros::Time(0), tf_temp);
+    }
+    catch (tf::TransformException ex){
+      //But, set the test back to true if an error was found.
+      test = true;
+      //Print the error and wait a second.
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+    }
+  }
+
+  //Initialize the position and orientation of g_pose with the origin and the rotation of this transform.
+  g_pose.pose.position.x = tf_temp.getOrigin().x();
+  g_pose.pose.position.y = tf_temp.getOrigin().y();
+  g_pose.pose.position.z = tf_temp.getOrigin().z();
+  g_pose.pose.orientation.x = tf_temp.getRotation().getAxis().x();
+  g_pose.pose.orientation.y = tf_temp.getRotation().getAxis().y();
+  g_pose.pose.orientation.z = tf_temp.getRotation().getAxis().z();
+  g_pose.pose.orientation.w = tf_temp.getRotation().getW();
+
+  //Create a broadcaster object
+  static tf::TransformBroadcaster br;
+
+  //Create a transform object
+  tf::Transform transform;
+
+  ros::Rate naptime(10);
+
   //Spin, listening for a published message
   while (ros::ok()){
-    ros::spin();
+    //Check for published message
+    ros::spinOnce();
+
+    //Set the origin of the transform using the x,y,z members of msg.pose.position. msg is the PoseStamped that's being passed into the callback funtion.
+    transform.setOrigin(tf::Vector3(g_pose.pose.position.x, g_pose.pose.position.y, 0.0));
+
+    //Set the orientation of the transform using the x,y,z,w members msg.pose.orientation. msg is the PoseStamped that's being passed into the callback funtion.
+    transform.setRotation(tf::Quaternion(g_pose.pose.orientation.x,g_pose.pose.orientation.y,g_pose.pose.orientation.z,g_pose.pose.orientation.w));
+
+    //Finally, broadcast the Transform. It is possible that this should be done over and over at a specific rate.
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(),"map_static","goalPose"));
+
+    //Sleep for rest of loop cyle
+    naptime.sleep();
   }
   return 0;
 }
--- src/simple_steering.cpp
+++ src/simple_steering.cpp
@@ -1,15 +1,14 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include <geometry_msgs/Pose.h>
 #include <tf/transform_listener.h>
 
 int main(int argc, char** argv){
+  //Initialize the node
   ros::init(argc, argv, "simple_steering");
   ros::NodeHandle nh;
 
-  ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/robot0/cmd_vel",1); //This is the advertiser object that will publish the desired forward and angular velocity of the robot.
-
-  tf::TransformListener listener;
+  //This is the advertiser object that will publish the desired forward and angular velocity of the robot.
+  ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/robot0/cmd_vel",1);
 
   // Initialize the velocity command that we will publish.
   geometry_msgs::Twist vel;
@@ -20,24 +19,32 @@ int main(int argc, char** argv){
   vel.angular.y = 0;
   vel.angular.z = 0;
 
+  //Create a TransformListener Object
+  tf::TransformListener listener;
 
+  //Create a Stamped Transform Object. This will be the transformation between the current robot pose and the goal pose.
   tf::StampedTransform transform;
 
   ros::Rate rate(10.0);
 
   while (nh.ok()){
       try{
-	listener.lookupTransform("/goalPose","/robot0",ros::Time(0),transform);
+        //Listen for the most recent transform (should use listener.waitForTransform)
+	listener.lookupTransform("/robot0","/goalPose",ros::Time(0),transform);
       }
       catch (tf::TransformException ex){
+	//If there is an exception, print error and sleep a second. This is dangerous. The velocity command should reset to zero.
 	ROS_ERROR("%s",ex.what());
 	ros::Duration(1.0).sleep();
       }
 
+      //Simple algorithm to steer robot to a point (not orientation).
       vel.angular.z = 4.0 * atan2(transform.getOrigin().y(),transform.getOrigin().x());
       vel.linear.x = 0.5 * sqrt(pow(transform.getOrigin().x(),2)+pow(transform.getOrigin().y(),2));
 
+      //Publish velocity command.
       vel_pub.publish(vel);
+      //And sleep for the rest of the loop cycle.
       rate.sleep();
     }
   return 0;
